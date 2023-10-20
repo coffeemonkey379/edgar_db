04dd65854f96c28f5556fb24dcd6a45650b2df10
Sam Thelwall <samuel.thelwall@fideres.com>
2023-10-20 11:29:41 +0100

    example log to console

--- examples/collect_reports.py
+++ examples/collect_reports.py
@@ -1,6 +1,15 @@
 import os
+from itertools import product
+import logging
 
 from edgar_db import collect_edgar, load_engine_url, build_cache
+from edgar_db.logger import LOGGER
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+prompt_handler = logging.StreamHandler()
+prompt_handler.setFormatter(formatter)
+prompt_handler.setLevel(logging.DEBUG)
+LOGGER.addHandler(prompt_handler)
 
 # Stored username & password in environment variables
 username, password = os.environ.get("LOCAL_DB").split(";")
@@ -9,6 +18,9 @@ db_url = load_engine_url(username, password, database="edgar_dev")
 build_cache(db_url)
 
 year = 2022
-quarter = 4
+quarter = 3
 
 collect_edgar(year, quarter, db_url)
+
+years = range(2014, 2022)
+quarters = range(4)
