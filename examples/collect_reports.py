f9e055aa6ef6274d70d1eb9efcd7d6455d540634
Sam Thelwall <samuel.thelwall@fideres.com>
2023-10-25 15:08:30 +0100

    add logs examples

--- examples/collect_reports.py
+++ examples/collect_reports.py
@@ -9,18 +9,23 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 prompt_handler = logging.StreamHandler()
 prompt_handler.setFormatter(formatter)
 prompt_handler.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logging.log")
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
 LOGGER.addHandler(prompt_handler)
+LOGGER.addHandler(file_handler)
 
+
+years = range(2013, 2024)
+quarters = range(1, 5)
 # Stored username & password in environment variables
 username, password = os.environ.get("LOCAL_DB").split(";")
 
-db_url = load_engine_url(username, password, database="edgar_dev")
+db_url = load_engine_url(username, password, database="edgar_dev_extra_types")
 build_cache(db_url)
 
-year = 2022
-quarter = 3
-
-collect_edgar(year, quarter, db_url)
-
-years = range(2014, 2022)
-quarters = range(4)
+for year, quarter in product(years, quarters):
+    try:
+        collect_edgar(year, quarter, db_url)
+    except:
+        pass
