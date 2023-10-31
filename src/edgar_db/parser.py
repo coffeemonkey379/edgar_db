f8c7c630a9ff4b337749311ec7b54816df0b1c03
Sam Thelwall <samuel.thelwall@fideres.com>
2023-10-31 19:07:40 +0000

    rename file

---
+++ src/edgar_db/parser.py
@@ -0,0 +1,197 @@
+from datetime import datetime
+from abc import abstractmethod, ABCMeta
+from typing import Type, Callable
+from collections import defaultdict
+
+from edgar_db.orm_db import (
+    Base,
+    SubmissionOrm,
+    CoverPageOrm,
+    OtherManagerOrm,
+    SignatureOrm,
+    SummaryPageOrm,
+    OtherManager2Orm,
+    InfoTableOrm,
+)
+
+
+class EdgarParser(metaclass=ABCMeta):
+    orm: Type[Base]
+
+    @abstractmethod
+    def parse_table_args(cls, *args, **kwargs) -> tuple[str]:
+        """_summary_
+
+        Returns:
+            Base: _description_
+        """
+        pass
+
+    @classmethod
+    def _parse_attribute_args(cls, attribute: str) -> tuple[str, bool]:
+        return (
+            getattr(cls.orm, attribute).type.python_type.__name__,
+            getattr(cls.orm, attribute).nullable,
+        )
+
+    @classmethod
+    def _build_table_args(cls, func_locals: dict[str, str]) -> tuple[str]:
+        vars: tuple[str] = cls.orm.__init__.__code__.co_varnames[1:-1]
+        parse_func: Callable[[str], str] = lambda var: cls._parse(
+            func_locals[var], *cls._parse_attribute_args(var)
+        )
+        table_args = tuple(map(parse_func, vars))
+        return table_args
+
+    @classmethod
+    def _parse(cls, str_: str, method: str, nullable: bool = False) -> str:
+        if (nullable) & (str_ == ""):
+            return str_
+        func_map: defaultdict[str, Callable[[str], str]] = defaultdict(
+            lambda: lambda x: x.replace("\\", "/")
+        )
+        func_map["date"] = cls.parse_date
+        res = func_map[method](str_)
+        return res
+
+    @classmethod
+    def parse_date(cls, date_str: str) -> str:
+        return datetime.strptime(date_str, "%d-%b-%Y").date().isoformat()
+
+
+class SubmissionParser(EdgarParser):
+    orm = SubmissionOrm
+
+    @classmethod
+    def parse_table_args(
+        cls,
+        accession_number: str,
+        filing_date: str,
+        submissiontype: str,
+        cik: str,
+        periodofreport: str,
+    ) -> tuple[str]:
+        func_locals = locals()
+        return cls._build_table_args(func_locals)
+
+
+class CoverPageParser(EdgarParser):
+    orm = CoverPageOrm
+
+    @classmethod
+    def parse_table_args(
+        cls,
+        accession_number: str,
+        reportcalendarorquarter: str,
+        isamendment: str,
+        amendmentno: str,
+        amendmenttype: str,
+        confdeniedexpired: str,
+        datedeniedexpired: str,
+        datereported: str,
+        reasonfornonconfidentiality: str,
+        filingmanager_name: str,
+        filingmanager_street1: str,
+        filingmanager_street2: str,
+        filingmanager_city: str,
+        filingmanager_stateorcountry: str,
+        filingmanager_zipcode: str,
+        reporttype: str,
+        form13ffilenumber: str,
+        provideinfoforinstruction5: str,
+        additionalinformation: str,
+    ) -> tuple[str]:
+        func_locals = locals()
+        return cls._build_table_args(func_locals)
+
+
+class OtherManagerParser(EdgarParser):
+    orm = OtherManagerOrm
+
+    @classmethod
+    def parse_table_args(
+        cls,
+        accession_number: str,
+        othermanager_sk: str,
+        cik: str,
+        form13ffilenumber: str,
+        name: str,
+    ) -> tuple[str]:
+        func_locals = locals()
+        return cls._build_table_args(func_locals)
+
+
+class SignatureParser(EdgarParser):
+    orm = SignatureOrm
+
+    @classmethod
+    def parse_table_args(
+        cls,
+        accession_number: str,
+        name: str,
+        title: str,
+        phone: str,
+        signature: str,
+        city: str,
+        stateorcountry: str,
+        signaturedate: str,
+    ) -> tuple[str]:
+        func_locals = locals()
+        return cls._build_table_args(func_locals)
+
+
+class SummaryPageParser(EdgarParser):
+    orm = SummaryPageOrm
+
+    @classmethod
+    def parse_table_args(
+        cls,
+        accession_number: str,
+        otherincludedmanagerscount: str,
+        tableentrytotal: str,
+        tablevaluetotal: str,
+        isconfidentialomitted: str,
+    ) -> tuple[str]:
+        func_locals = locals()
+        return cls._build_table_args(func_locals)
+
+
+class OtherManager2Parser(EdgarParser):
+    orm = OtherManager2Orm
+
+    @classmethod
+    def parse_table_args(
+        cls,
+        accession_number: str,
+        sequencenumber: str,
+        cik: str,
+        form13ffilenumber: str,
+        name: str,
+    ) -> tuple[str]:
+        func_locals = locals()
+        return cls._build_table_args(func_locals)
+
+
+class InfoTableParser(EdgarParser):
+    orm = InfoTableOrm
+
+    @classmethod
+    def parse_table_args(
+        cls,
+        accession_number: str,
+        infotable_sk: str,
+        nameofissuer: str,
+        titleofclass: str,
+        cusip: str,
+        value: str,
+        sshprnamt: str,
+        sshprnamttype: str,
+        putcall: str,
+        investmentdiscretion: str,
+        othermanager: str,
+        voting_auth_sole: str,
+        voting_auth_shared: str,
+        voting_auth_none: str,
+    ) -> tuple[str]:
+        func_locals = locals()
+        return cls._build_table_args(func_locals)
--- src/edgar_db/upload.py
+++ src/edgar_db/upload.py
@@ -1,12 +1,11 @@
-from typing import Generator, Type, Iterable
 from zipfile import ZipFile
 
 from sqlalchemy import URL
-import psycopg2
 
 from edgar_db.logger import LOGGER
-from edgar_db.orm_parser import OrmParser
+from edgar_db.parser import EdgarParser
 from edgar_db.bulk_insert import bulk_upload
+from edgar_db.orm_db import build_psycopg_conn
 
 
 class MissingFileError(Exception):
@@ -17,12 +16,13 @@ class OrmOrderError(Exception):
     pass
 
 
-class BuildZipOrm:
+class ZipUploader:
     """
+
 
     Args:
         zip_file (ZipFile): _description_
-        file_orm_map (dict[str, OrmParser]): _description_
+        file_orm_map (dict[str, EdgarParser]): _description_
 
     Raises:
         MissingFileError: _description_
@@ -31,13 +31,13 @@ class BuildZipOrm:
     def __init__(
         self,
         zip_file: ZipFile,
-        file_orm_map: dict[str, OrmParser],
+        file_orm_map: dict[str, type[EdgarParser]],
         engine_str: URL,
     ):
         self.self = self
         self.zip_file = zip_file
         self.file_orm_map = file_orm_map
-        self.connection = self._build_psycopg_conn(engine_str)
+        self.connection = build_psycopg_conn(engine_str)
 
         file_names = list(map(lambda x: x.filename, self.zip_file.infolist()))
 
@@ -53,39 +53,26 @@ class BuildZipOrm:
             self._upload(file, orm_map)
             LOGGER.info(f"Finished upload on {file}")
 
-    def _insert(self, values, parser: OrmParser) -> None:
+    def _insert(self, values, parser: type[EdgarParser]) -> None:
         bulk_upload(
             self.connection,
             parser.orm.__tablename__,
             map(self._decode_line, values),
-            parser,
+            parser.parse_table_args,
         )
 
-    def _build_psycopg_conn(self, engine_str: URL) -> psycopg2.extensions.connection:
-        kwargs = {}
-        kwargs["host"] = engine_str.host
-        kwargs["user"] = engine_str.username
-        kwargs["password"] = engine_str.password
-        kwargs["database"] = engine_str.database
-        if engine_str.port:
-            kwargs["port"] = engine_str.port
-        connection = psycopg2.connect(**kwargs)
-        connection.autocommit = True
-
-        return connection
-
-    def _upload(self, file: str, orm_parser: OrmParser) -> None:
+    def _upload(self, file: str, parser: type[EdgarParser]) -> None:
         file_value = self.zip_file.open(file)
         headers = self._decode_line(file_value.readline(), lower=True)
-        table_args_order = list(orm_parser.parse_table_args.__code__.co_varnames[1:-1])
+        table_args_order = list(parser.parse_table_args.__code__.co_varnames[1:-1])
         if table_args_order != headers:
             args_order = "\n".join(table_args_order)
             header_order = "\n".join(headers)
             raise OrmOrderError(
-                f"\n\nOrder of the ORM {orm_parser.orm} initialisation parameters: \n\n{args_order}\n\n\n"
+                f"\n\nOrder of the ORM {parser.orm} initialisation parameters: \n\n{args_order}\n\n\n"
                 f"is not equal to file {file} header order: \n\n{header_order}"
             )
-        self._insert(file_value, orm_parser)
+        self._insert(file_value, parser)
 
     def _decode_line(self, line: bytes, lower: bool = False) -> list[str]:
         decoded = line.decode()
