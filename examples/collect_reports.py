84202554db1dcb13a77cbc0e3f0a908e80d51426
Sam Thelwall <samuel.thelwall@fideres.com>
2023-10-31 19:05:17 +0000

    update examples

--- examples/collect_reports.py
+++ examples/collect_reports.py
@@ -5,6 +5,7 @@ import logging
 from edgar_db import collect_edgar, load_engine_url, build_cache
 from edgar_db.logger import LOGGER
 
+
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 prompt_handler = logging.StreamHandler()
 prompt_handler.setFormatter(formatter)
@@ -16,16 +17,17 @@ LOGGER.addHandler(prompt_handler)
 LOGGER.addHandler(file_handler)
 
 
-years = range(2013, 2024)
+years = range(2014, 2024)
 quarters = range(1, 5)
 # Stored username & password in environment variables
 username, password = os.environ.get("LOCAL_DB").split(";")
 
-db_url = load_engine_url(username, password, database="edgar_dev_extra_types")
-build_cache(db_url)
+db_url = load_engine_url(
+    username,
+    password,
+    database="edgar_13f",
+)
+db_url = build_cache(db_url)
 
 for year, quarter in product(years, quarters):
-    try:
-        collect_edgar(year, quarter, db_url)
-    except:
-        pass
+    collect_edgar(year, quarter, db_url)
