d89d8a1a9dd68c5c52ea851c69a8619c0ff9a044
Sam Thelwall <samuel.thelwall@fideres.com>
2023-10-31 19:07:20 +0000

    decouple url build

--- src/edgar_db/urls.py
+++ src/edgar_db/urls.py
@@ -8,7 +8,7 @@ from sqlalchemy import URL
 from edgar_db.logger import LOGGER
 
 
-def build_sec_url(year: int, quarter: Literal[1, 2, 3, 4]) -> str:
+def build_sec_13f_url(year: int, quarter: Literal[1, 2, 3, 4]) -> str:
     """Builds URL for SEC 13-f zip file.
 
     Args:
@@ -22,11 +22,16 @@ def build_sec_url(year: int, quarter: Literal[1, 2, 3, 4]) -> str:
     return f"{sec_url}{year}q{quarter}_form13f.zip"
 
 
+def build_sec_securities_url(year: int, quarter: Literal[1, 2, 3, 4]) -> str:
+    sec_url = "https://www.sec.gov/divisions/investment/13f/"
+    return f"{sec_url}13flist{year}q{quarter}.pdf"
+
+
 class HttpError(Exception):
     pass
 
 
-def collect_url_zip(url: str) -> ZipFile:
+def open_url(url: str) -> BytesIO:
     """Collects zip file from URL.
 
     Args:
@@ -41,8 +46,26 @@ def collect_url_zip(url: str) -> ZipFile:
     LOGGER.info(f"Starting url open {url}")
     with urlopen(url) as response:
         content = response.read()
-    file = ZipFile(BytesIO(content))
-    LOGGER.info(f"Starting opened url zip {url}")
+    LOGGER.info(f"Finished url open {url}")
+    bytes_io = BytesIO(content)
+
+    return bytes_io
+
+
+def collect_url_zip(url: str) -> ZipFile:
+    """Collects zip file from URL.
+
+    Args:
+        url (str): URL containing zip file.
+
+    Raises:
+        HttpError: URL response status not 200
+
+    Returns:
+        ZipFile: zip file from URL.
+    """
+    file = ZipFile(open_url(url))
+
     return file
 
 
